7b7497c2aaefaf79d6467a1f8410833a4c36c180
Rudy Zhou <Rudy.Zhou@td.com>
2018-12-11 13:45:14 -0500

    Convert main menu from ListView to RecyclerView

---
+++ app/src/main/java/com/test/zhouq2/simpleapp/NoteRecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.test.zhouq2.simpleapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
+    private final Context mContext;
+    private final List<NoteInfo> mNotes;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecyclerAdapter(Context mContext, List<NoteInfo> mNotes) {
+        this.mContext = mContext;
+        this.mNotes = mNotes;
+        mLayoutInflater = LayoutInflater.from(mContext);
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        NoteInfo note = mNotes.get(position);
+        holder.mTextCourse.setText(note.getCourse().getTitle());
+        holder.mTextTitle.setText(note.getTitle());
+        holder.mCurrPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+        public int mCurrPosition;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mContext, NoteActivity.class);
+                    intent.putExtra(NoteActivity.NOTE_POSITION, mCurrPosition);
+                    mContext.startActivity(intent);
+                }
+            });
+        }
+    }
+}
