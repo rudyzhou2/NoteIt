cf1a10af7824053f69a954bfd2c70019c007faeb
Rudy Zhou <Rudy.Zhou@td.com>
2018-12-13 18:30:39 -0500

    Added UI RecyclerView test

---
+++ app/src/androidTest/java/com/test/zhouq2/simpleapp/NextThroughNotesTest.java
@@ -0,0 +1,61 @@
+package com.test.zhouq2.simpleapp;
+
+import android.provider.ContactsContract;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.*;
+import android.support.v7.widget.RecyclerView;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class NextThroughNotesTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void NextThroughNotes() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        for(int idx = 0; idx < notes.size(); idx++) {
+            NoteInfo note = notes.get(idx);
+
+            onView(withId(R.id.spinner_courses)).check(
+                    matches(withSpinnerText(note.getCourse().getTitle()))
+            );
+            onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+            onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+            if (idx < notes.size() - 1)
+                onView(allOf(withId(R.id.action_next), isEnabled())).perform(click());
+        }
+        onView(withId(R.id.action_next)).check(matches(not(isEnabled())));
+
+    }
+}
