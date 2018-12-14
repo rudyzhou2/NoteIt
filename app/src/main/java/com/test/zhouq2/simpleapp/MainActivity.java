82a38d5feb07a643e9c4b0fe90de13ff6bb5188e
Rudy Zhou <Rudy.Zhou@td.com>
2018-12-14 14:54:06 -0500

    Added drawable icons, fixed hard coded strings/values

--- app/src/main/java/com/test/zhouq2/simpleapp/MainActivity.java
+++ app/src/main/java/com/test/zhouq2/simpleapp/MainActivity.java
@@ -99,9 +99,9 @@ public class MainActivity extends AppCompatActivity
             displayCourses();
             // handleSelection("Courses");
         } else if (id == R.id.nav_share) {
-            handleSelection("Share");
+            handleSelection(R.string.nav_share_msg);
         } else if (id == R.id.nav_send) {
-            handleSelection("Send");
+            handleSelection(R.string.nav_send_msg);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -109,9 +109,9 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void handleSelection(String message) {
+    private void handleSelection(int message_id) {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, message_id, Snackbar.LENGTH_LONG).show();
     }
 
     @Override
@@ -124,7 +124,7 @@ public class MainActivity extends AppCompatActivity
     private void initializeDisplayContent() {
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
         mNotesLayoutManager = new LinearLayoutManager(this);
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        mCoursesLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.course_grid_span));
 
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
